5fd49f330fbb7302d230683ddf0842b2966d9fcb
Alexander <myagkiyalexandr@hotmail.com>
2019-05-06 11:44:45 +0300

    Helper recognizes speech
    * Forgotten files added

---
+++ src/app/recognition-status/recognition-status.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recognition-status',
+  template: `
+    <span class="wrapper">
+    </span>
+  `,
+  styleUrls: ['./recognition-status.component.scss']
+})
+export class RecognitionStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
